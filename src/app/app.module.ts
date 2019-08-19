de9a3990d7b640edd54790d2444dac422087b2ce
Morolari Titilope <tmorolari@gmail.com>
2019-08-19 07:29:54 +0100

    feat(UI): generate and display random numbers
    [Delivers #167964577]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {PhoneNumberGeneratorComponent} from './phonenumber-generator/phonenumber-generator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhoneNumberGeneratorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
